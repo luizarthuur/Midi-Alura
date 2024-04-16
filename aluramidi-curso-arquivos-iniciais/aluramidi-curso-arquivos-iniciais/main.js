9b776f03de7564c0227f0dd0064431ec1d34060d
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-04-16 18:25:34 -0300

    criação e finalização do projeto

--- aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,11 +1,62 @@
-function tocaSomPom () {
+function tocaSom (idElementoAudio) {
 
-    document.querySelector('#som_tecla_pom').play();
+    const elemento = document.querySelector(idElementoAudio).play();
+
+    if  (elemento != null && elemento.localname === 'audio') {
+        elemento.play();
+    }
+
+    else {
+        console.log('O elemento não foi encontrado ou seletor inválido');
+    }
 
 }
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let cont = 0;
+
+while (cont < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[cont]
+    const instrumento = tecla.classList[1]
+
+    tecla.onclick = function () {
+        tocaSom(`#som_${instrumento}`);
+    }
+
+    cont ++
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
 
 
-document.querySelector('.tecla_pom');
+// event.code tras as informações do que está acontecendo na página em tempo real a partir de uma função com o parametro event
 
-tocaSomPom();
+//listaDeTeclas[0].classList class list serve para pegar as informações de todas as classes do item (de acordo com o indice)
+/*
+
+mesma coisa, utilizando o for
+
+for (let contador = 0; contador < listaDeTeclas.length; cont++) {
+
+    const tecla = listaDeTeclas[cont]
+    const instrumento = tecla.classList[1]
+
+    tecla.onclick = function () {
+        tocaSom(`#som_${instrumento}`);
+    }
+
+}
+*/
+
